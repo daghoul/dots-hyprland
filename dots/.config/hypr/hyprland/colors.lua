4bee25c6deb00e241138526ef5100cfe1c13887e
daghoul <>
2026-08-01 21:32:45 +0800

    update gitignore

---
+++ dots/.config/hypr/hyprland/colors.lua
@@ -0,0 +1,16 @@
+hl.config({
+    general = {
+        col = {
+            active_border   = "rgba(4a464377)",
+            inactive_border = "rgba(1e1b1933)",
+        },
+    },
+    misc = {
+        background_color = "rgba(151311FF)",
+    },
+})
+
+hl.window_rule({
+    match        = { pin = 1 },
+    border_color = "rgba(dac3acAA) rgba(dac3ac77)",
+})
